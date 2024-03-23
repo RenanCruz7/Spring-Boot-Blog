4fe3109d1d27edc9e57cba7b7474d71958722cd1
RenanCruz <renancruz199@gmail.com>
2024-03-23 12:35:08 -0300

    Commits

---
+++ src/test/java/sping/bot/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package sping.bot.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
