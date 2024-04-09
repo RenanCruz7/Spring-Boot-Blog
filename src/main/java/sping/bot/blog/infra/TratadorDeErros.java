99c90a3f8e21b44774dfc7cc31e62ff7c364abe4
RenanCruz <renancruz199@gmail.com>
2024-04-08 23:01:39 -0300

    Tratando erros

---
+++ src/main/java/sping/bot/blog/infra/TratadorDeErros.java
@@ -0,0 +1,39 @@
+package sping.bot.blog.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+// Geralmente, quando uma entidade não é encontrada, o Spring lança uma exceção do tipo EntityNotFoundException.
+// Para tratar esse erro, criamos um tratador de erros que intercepta a exceção EntityNotFoundException e retorna um status 404.
+// Para isso, criamos uma classe chamada TratadorDeErros e anotamos com @RestControllerAdvice.
+// Em seguida, criamos um método chamado tratarErro404 que retorna um ResponseEntity com status 404.
+// Por fim, anotamos o método com @ExceptionHandler e passamos a exceção EntityNotFoundException como parâmetro.
+// Dessa forma, sempre que uma exceção EntityNotFoundException for lançada, o método tratarErro404 será chamado e retornará um status 404.
+// Assim, garantimos que a resposta da API seja coerente com o erro ocorrido.
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+
+    }
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
