b3832bd774b4b54b3cc98e9fc00bc62559774e8f
RenanCruz <renancruz199@gmail.com>
2024-04-10 20:00:53 -0300

    Repository e Service

--- src/main/java/sping/bot/blog/domain/post/PostRepository.java
+++ src/main/java/sping/bot/blog/domain/post/PostRepository.java
@@ -2,5 +2,10 @@ package sping.bot.blog.domain.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Repository é uma interface que estende JpaRepository
+// JpaRepository é uma interface que possui métodos para manipular dados no banco de dados
+// JpaRepository recebe dois parâmetros, o tipo da entidade e o tipo do id da entidade
+// Neste caso, a entidade é Post e o id é Long
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
